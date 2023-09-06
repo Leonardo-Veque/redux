e7322706e21c13df4d54ff295517b4d29f66bd3a
Tiago Lima <tiagoepitanga10@gmail.com>
2023-09-05 21:59:38 -0300

    Estilização pronta

--- src/Counter.js
+++ src/Counter.js
@@ -3,11 +3,15 @@ import { connect } from 'react-redux';
 
 const Counter = (props) => {
     return(
-        <div>
-            <h1>Contator Redux</h1>
-            <p>Contagem: {props.count}</p>
-            <button onClick={props.incremento}>Adicionar</button>
-            <button onClick={props.decremento}>Desadicionar</button>
+        <div className='w-screen h-screen flex flex-col bg-zinc-400 items-center justify-center'>
+            <div className='flex-col flex bg-zinc-500 items-center rounded-xl w-4/12'>
+                <h1>Contator Redux</h1>
+                <h2 className='font-semibold text-lg p-6'>Contagem: {props.count}</h2>
+                <div className='flex flex-row items-center justify-center pb-4 gap-10'>
+                    <button onClick={props.incremento} className='px-4 py-2 bg-green-500 rounded' >Adicionar</button>
+                    <button onClick={props.decremento} className='px-4 py-2 bg-red-500 rounded' >Desadicionar</button>
+                </div>
+            </div>
         </div>
     );
 
